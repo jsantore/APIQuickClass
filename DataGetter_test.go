b46b3534db73fcabafd2dd3903d0e9896bdb0f1a
jsantore <jsantore@bridgew.edu>
2020-11-17 19:08:57 -0500

    try another demo commit

--- DataGetter_test.go
+++ DataGetter_test.go
@@ -10,10 +10,14 @@ func TestGetWeatherData(t *testing.T) {
 	if answer.StatusCode != 200 {
 		t.Error("Got bad Status")
 	}
+	if 1 > 2 {
+
+	}
 	data, err := ioutil.ReadAll(answer.Body)
 	if err != nil {
 		t.Error("error reading")
 	}
+	//trying stuff out
 	if len(data) < 1 {
 		t.Error("there was no data")
 	}
